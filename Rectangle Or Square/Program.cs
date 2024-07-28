64dfdf7e513005dca0151c0a8aa0acd5d5b9ab1b
ahmed abnaser <ahmeducfjf@gmail.com>
2024-07-28 06:26:48 +0300

    Add project files.

---
+++ Rectangle Or Square/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Rectangle_Or_Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pls enter the width and the length of your shape");
+            int width, length;
+            width = int.Parse(Console.ReadLine());
+            length = int.Parse(Console.ReadLine());
+            if (width == length)
+                Console.WriteLine("this is a square");
+            else
+                Console.WriteLine("this is a rectangle");
+        }
+    }
+}
